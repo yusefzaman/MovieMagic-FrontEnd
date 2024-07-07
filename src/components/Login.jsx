b0b5a332b7bacea39fd6eb0b210c109cc700fd71
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-07 03:55:45 -0700

    auth

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { login } from '../services/authService'
+
+const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  
+}
+
+export default Login
