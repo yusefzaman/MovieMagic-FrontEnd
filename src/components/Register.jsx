c43ddbaddc0296d3101be85385471f927975c54d
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-07 04:05:09 -0700

    auth

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 import { register } from '../services/authService'
 
 const Register = () => {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [message, setMessage] = useState("")
-
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
 }
 
-export default Register;
+return (
+  <div>
+    <h2>Register</h2>
+  </div>
+)
+
+export default Register
