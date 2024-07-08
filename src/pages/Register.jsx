2f893f00439f0a0772277b763ff0f9c037c4aaa0
hussain <hussainaradi.ha@gmail.com>
2024-07-09 00:07:45 +0300

    add the Register NavLink to the Nav

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -25,4 +25,66 @@ const Register = () => {
       setError('Registration failed')
     }
   }
+  return (
+    <div className="register">
+      <div className="registerCard">
+        <form c onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="name">Name</label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              name="name"
+              type="text"
+              placeholder="John Smith"
+              value={name}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={email}
+              required
+            />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              name="password"
+              value={password}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              required
+            />
+          </div>
+          {error && <p className="error">{error}</p>}
+          <button
+            type="submit"
+            disabled={
+              !name || !email || !password || password !== confirmPassword
+            }
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
+
+export default Register
