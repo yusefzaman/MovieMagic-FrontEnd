a2bdf108d476f03abcd2938163cdec21037510a1
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-08 10:53:17 -0700

    review

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css';
-import { Route, Routes} from 'react-router-dom';
-import Nav from './components/Nav';
-import Home from './pages/Home';
-import About from './pages/About';
-import Movie from './pages/Movie';
-import Theatre from './pages/Theatre';
-import ShowTime from './pages/ShowTime';
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import About from './pages/About'
+import Movie from './pages/Movie'
+import Theatre from './pages/Theatre'
+import ShowTime from './pages/ShowTime'
 import SignIn from './pages/SignIn'
-import Register from './pages/Register';
+import Register from './pages/Register'
+import ReviewForm from '../components/ReviewForm'
+import ReviewList from '../components/ReviewList'
 const App = () => {
   return (
     <div className="App">
@@ -22,9 +24,15 @@ const App = () => {
           <Route path="/Seats" element={<ShowTime />} />
           <Route path="/signin" element={SignIn} />
           <Route path="/register" element={Register} />
+          <ReviewForm
+            movieId={movieId}
+            userId={userId}
+            onReviewSubmitted={handleReviewSubmitted}
+          />
+          <ReviewList movieId={movieId} />
         </Routes>
       </main>
-      </div>
+    </div>
   )
 }
 
