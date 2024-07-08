c7bbb027dd6d5f8c8bcaaed716a3e218e8c31f5a
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-07 22:09:59 -0700

    auth

--- src/services/Auth.jsx
+++ src/services/Auth.jsx
@@ -8,4 +8,15 @@ export const register = async (name, email, password) => {
     },
     body: JSON.stringify({ name, email, password })
   })
+  return response.json()
+}
+
+export const login = async (email, password) => {
+  const response = await fetch(`${API_URL}/users/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
 }
