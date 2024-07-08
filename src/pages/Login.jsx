a2a57b7f969fcfda1519f3798c62fdb66e11cb6c
hussain <hussainaradi.ha@gmail.com>
2024-07-09 00:41:48 +0300

    initilize the useState for the Login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { useState } from "react";
+const Login=()=>{
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+  const [error,setError]=useState('')
+  
+}
