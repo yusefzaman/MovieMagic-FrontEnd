a0647e26c00a2c1b9971563d9898dd996155ee07
hussain <hussainaradi.ha@gmail.com>
2024-07-08 23:55:49 +0300

    initlize the useState

---
+++ src/pages/Register.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import { useState } from 'react'
+const Register = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+}
