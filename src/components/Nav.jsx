b580d5c949384e47a051b438044cece018dfb902
yusefzaman <yusef96@gmail.com>
2024-07-09 09:05:00 +0300

    update of home component css layout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,14 @@
 import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
-
-    return (
-        <header>
-          <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/About">About</NavLink>
-          <NavLink to="/signin">Sign In</NavLink>
-          </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/About">About</NavLink>
+        <NavLink to="/signin">Sign In</NavLink>
+      </nav>
+    </header>
+  )
 }
 export default Nav
