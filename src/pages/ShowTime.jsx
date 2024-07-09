c00eb4edf8997ea1b4e4af6a1a682cb9c6778770
Mohamed Husain <mo.abdulla3000@gmail.com>
2024-07-09 05:14:42 -0700

    finish the theatre section at the frontEnd and movies

--- src/pages/ShowTime.jsx
+++ src/pages/ShowTime.jsx
@@ -5,6 +5,11 @@ const ShowTime = () => {
   const [user, setUser] = useState('')
   const [number, setNumber] = useState(0)
   const [seats, setSeats] = useState([])
+
+  
+
+
+
 }
 
 export default ShowTime
