c38257d58bfadf42de9d7784f509d3b65ec14881
Mohamed Husain <mo.abdulla3000@gmail.com>
2024-07-10 08:35:41 -0700

    developing showtime

--- src/pages/Theatre.jsx
+++ src/pages/Theatre.jsx
@@ -8,18 +8,37 @@ const Theatre = () => {
   const { movieName } = useParams();
 
   useEffect(() => {
+    console.log("response");
+
     getTheatres()
+    // Theatres()
   }, [movieName])
 
 
   const getTheatres = async () => {
     try {
       const response = await axios.post(`http://localhost:5000/fetch_theatres`,{movieName: movieName})
+      // console.log("response",response.data);
+      // const data= response.data.map(theatre=>{
+      //   theatre.time=theatre.time.slice(1,theatre.time.length-1).split(",")
+      //   return theatre
+      // })
+
       setTheatres(response.data.data)
     } catch (error) {
       console.error('Error fetching data:', error)
     }
   }
+
+  // const Theatres = async () => {
+  //   try {
+  //     const response = await axios.post(`http://localhost:5000/theatres`,{movieName: movieName})
+  //     console.log("response",response.data);
+  //     setTheatres(response.data)
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error)
+  //   }
+  // }
 
   return (
     <div className="Theatres">
@@ -32,7 +51,7 @@ const Theatre = () => {
             <p>Location: {theatre.location}</p>
             <ul>
               {theatre.time.map((time, index) => (
-                <Link to ={`${theatre._id}/Seats`}><li key={index}>{time}</li></Link>
+                <Link to ={`${theatre.id}/Seats`}><li key={index}>{time}</li></Link>
               ))}
             </ul>
           </div>
